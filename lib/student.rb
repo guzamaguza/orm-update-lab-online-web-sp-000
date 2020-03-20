4fcdebcbda6f35b17578b341d7b47766a79b329e
Mark A Guza <guzamaguza@gmail.com>
2020-03-20 18:34:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,13 +32,17 @@ class Student
 
   def save
   #this method really persists the student instance copy to the database
-      sql = <<-SQL
-        INSERT INTO students (name, grade)
-        VALUES (?, ?)
-      SQL
+      if self.id
+          self.update
+      else
+          sql = <<-SQL
+            INSERT INTO students (name, grade)
+            VALUES (?, ?)
+          SQL
 
-      DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+          DB[:conn].execute(sql, self.name, self.grade)
+          @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      end
   end
 
   def self.create(name:, grade:)
