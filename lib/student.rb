97f60ec9837cbe2afa56c409d6535699f7bc8044
Mark A Guza <guzamaguza@gmail.com>
2020-03-20 18:27:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,5 +10,17 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+    #below is a HEREDOC
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)  #execute SQL statement on database table
+  end
 
 end
