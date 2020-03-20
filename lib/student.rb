93105020ca5ef06a9435fcb5f91f49e47c153fcd
Mark A Guza <guzamaguza@gmail.com>
2020-03-20 18:47:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,7 +76,7 @@ class Student
     end
 
     def update
-        sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+        sql = "UPDATE students SET name = ?, album = ? WHERE id = ?"
         DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
 
